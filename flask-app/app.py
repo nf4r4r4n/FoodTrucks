5ec1a8b2bd0b3d99009ff79ab819bf18fd0d2ba8
nf4r4r4n <baibs.fararano@gmail.com>
2025-08-08 23:56:51 +0300

    compose updates

--- flask-app/app.py
+++ flask-app/app.py
@@ -49,6 +49,10 @@ def check_and_load_index():
 def index():
     return render_template('index.html')
 
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
 @app.route('/debug')
 def test_es():
     resp = {}
